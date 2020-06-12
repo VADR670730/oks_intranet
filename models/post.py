aab63cae87ebe7a0403a9975455f8f2ec99ec276
CrazyBolillo <random@email.com>
2020-06-12 12:51:56 -0500

    Made post kanban's larger. Module is no longer enabled by default for all users

--- models/post.py
+++ models/post.py
@@ -8,26 +8,20 @@ class IntranetPost(models.Model):
     def _get_today_date(self):
         return fields.Date.today()
 
-    @api.multi
-    def _compute_category_name(self):
-        for record in self:
-            record.category_name = record.category.name
-
-    @api.multi
+    @api.model
     def _compute_dev_view(self):
-        for record in self:
             if self.env.user.has_group("oks_intranet.intranet_manager"):
-                record.dev_view = True
+                return True
             else:
-                record.dev_view = False
+                return False
 
     name = fields.Char(string="Titulo", required=True)
     category = fields.Many2one(string="Categoria", comodel_name="oks.intranet.post.category", required=True)
-    category_name = fields.Char(compute=_compute_category_name, store=False)
+    category_name = fields.Char(related="category.name", store=False, readonly=True)
     description = fields.Char(string="Información")
     date = fields.Date(string="Publicado", readonly=True, default=_get_today_date)
     image = fields.Binary(string="Foto", attachment=True)
-    dev_view = fields.Boolean(compute=_compute_dev_view, store=False)
+    dev_view = fields.Boolean(default=_compute_dev_view, store=False)
 
     @api.model
     def create(self, vals):
